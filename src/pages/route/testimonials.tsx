b00790fdf0ce5b9cfb952a4cb3a5047ffea82524
Anshu Naagar <108215633+forge-of-creation@users.noreply.github.com>
2024-07-11 09:08:24 +0000

    Error: React Hook "useRouter" is called in function "testimonials" that is neither a React function component nor a custom React Hook function. React component names must start with an uppercase letter. React Hook names must start with the word "use".  react-hooks/rules-of-hooks fix

--- src/pages/route/testimonials.tsx
+++ src/pages/route/testimonials.tsx
@@ -1,21 +1,9 @@
-import axios from 'axios'
-import {useRouter} from 'next/router'
 import React from 'react'
 
-const testimonials = (props: any) => {
-  const {pathname} = useRouter()
-  console.log(pathname)
-  return <div>testimonials {props.response}</div>
+const testimonials = () => {
+  return (
+    <div>testimonials</div>
+  )
 }
 
 export default testimonials
-
-export async function getServerSideProps() {
-  const getCall = await axios.get('http://localhost:3000/api/sanity')
-  console.log(getCall)
-  return {
-    props: {
-      response: 2,
-    },
-  }
-}
