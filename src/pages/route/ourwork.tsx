7345d3167073bfd02fae47ced5b502cfa158235a
Anshu Naagar <andyhustleup47us@gmail.com>
2024-07-17 17:23:23 +0000

    Initialise our work page

--- src/pages/route/ourwork.tsx
+++ src/pages/route/ourwork.tsx
@@ -1,8 +1,35 @@
+import {manrope} from '@/utils/fonts'
 import React from 'react'
+import {HiAdjustmentsHorizontal} from 'react-icons/hi2'
 
 const ourwork = () => {
   return (
-    <div>ourwork</div>
+    <main className="ourwork-main-page">
+      <section className="header-main-section h-[85vh]   w-full max-h-[150vh] overflow-x-hidden flex flex-col text-start items-start  justify-center px-8">
+        <div className=" border border-black text-magery bg-white w-full py-8">
+          <h1
+            className={`${manrope.className} text-7xl font-bold 
+        `}
+          >
+            "Creativity is intelligence, <br /> - Have fun with it"
+          </h1>
+          <span className="text-3xl font-medium mt-8 float-right">
+            {' '}
+            <span></span> Albert Einstein
+          </span>
+        </div>
+
+        <img
+          src="/svg/astro.svg"
+          className="absolute top-0 left-0 w-full -z-10 h-full object-cover"
+          alt=""
+        />
+      </section>
+
+      <section className="h-[90vh]"></section>
+      <section className="h-[90vh]"></section>
+      <section className="h-[90vh]"></section>
+    </main>
   )
 }
 
--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -5,43 +5,51 @@ import {
   Lexend_Deca,
   Lexend_Giga,
   Raleway,
-  Quicksand
-} from "next/font/google";
+  Quicksand,
+  Manrope,
+} from 'next/font/google'
+
+const manrope = Manrope({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-manrope',
+  display: 'swap',
+})
 
 const quicksand = Quicksand({
-  weight: ["400", "700"],
-  subsets: ["latin"],
-  variable: "--font-quicksand",
-});
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-quicksand',
+})
 
 const raleway = Raleway({
-  weight: ["400", "700"],
-  subsets: ["latin"],
-  variable: "--font-raleway",
-});
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-raleway',
+})
 
 const inter = Inter({
-  variable: "--font-inter",
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
-});
+  variable: '--font-inter',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+})
 
 const poppins = Poppins({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
-  variable: "--font-poppins",
-});
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+  variable: '--font-poppins',
+})
 
 const sansita = Sansita({
-  weight: ["400", "700"],
-  subsets: ["latin"],
-  variable: "--font-sansita",
-});
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-sansita',
+})
 
 const lexendDeca = Lexend_Deca({
-  weight: ["400", "700"],
-  subsets: ["latin"],
-  variable: "--font-lexend-deca",
-});
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-lexend-deca',
+})
 
-export { inter, poppins, sansita, lexendDeca , raleway, quicksand};
+export {inter, poppins, sansita, lexendDeca, raleway, quicksand, manrope}
